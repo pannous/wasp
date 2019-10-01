e1ea858b0242d6a2ee4b6c137ba006564722192d
Pannous <info@pannous..com>
2019-10-01 14:12:45 +0200

    mark

---
+++ Fetch.cpp
@@ -0,0 +1,30 @@
+#include <curl/curl.h>
+#include <curl/easy.h>
+#include <sstream>
+#include <iostream>
+using namespace std;
+typedef const char *chars;
+size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream) {
+	string data((const char*) ptr, (size_t) size * nmemb);
+	*((stringstream*) stream) << data << endl;
+	return size * nmemb;
+}
+
+extern "C" chars fetch(chars url) {
+	std::stringstream out;
+	auto curl = curl_easy_init();
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+	curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1); //Prevent "longjmp causes uninitialized stack frame" bug
+	curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "deflate");
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &out);
+	CURLcode res = curl_easy_perform(curl);
+	if (res != CURLE_OK)
+		fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+	char *copy = (char *) malloc(out.str().length());
+	sprintf(copy,"%s",out.str().data());
+//	strcpy(copy, data);
+//	printf("%s", copy);
+	return copy;
+}
---
+++ Node.h
@@ -0,0 +1,14 @@
+//
+// Created by me on 30.09.19.
+//
+
+#ifndef MARK_NODE_H
+#define MARK_NODE_H
+
+
+class Node {
+
+};
+
+
+#endif //MARK_NODE_H
