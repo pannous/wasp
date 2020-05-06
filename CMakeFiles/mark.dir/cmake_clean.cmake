1fba06d8355214ea1ac99543434a4c1cacb2147d
Pannous <info@pannous.net>
2020-05-06 13:40:14 +0200

    CMakeLists.txt

---
+++ CMakeFiles/mark.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mark.dir/ErrorHandler.cpp.o"
+  "CMakeFiles/mark.dir/mark.cpp.o"
+  "mark"
+  "mark.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
