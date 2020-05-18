58549d59a9da90c0e2c622467c133a612f7974cc
Pannous <info@pannous.net>
2020-05-18 10:47:11 +0200

    ALL TESTS PASSING This time for real. HOW did they pass before? Will never be known.

--- CMakeFiles/mark.dir/DependInfo.cmake
+++ CMakeFiles/mark.dir/DependInfo.cmake
@@ -4,8 +4,8 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/me/dev/apps/mark/Angle.cpp" "/Users/me/dev/apps/mark/CMakeFiles/mark.dir/Angle.cpp.o"
   "/Users/me/dev/apps/mark/ErrorHandler.cpp" "/Users/me/dev/apps/mark/CMakeFiles/mark.dir/ErrorHandler.cpp.o"
-  "/Users/me/dev/apps/mark/mark.cpp" "/Users/me/dev/apps/mark/CMakeFiles/mark.dir/mark.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
