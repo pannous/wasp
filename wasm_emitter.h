7ca22a924a9b405663d76169406c5d1d848c5966
Pannous <info@pannous..com>
2020-12-08 16:42:17 +0100

    signatures ok

--- wasm_emitter.h
+++ wasm_emitter.h
@@ -9,33 +9,17 @@
 
 typedef const unsigned char* wasm_string;// wasm strings start with their length and do NOT end with 0 !! :(
 typedef unsigned char* bytes;
-Code& unsignedLEB128(long n);
-Code& signedLEB128(long value);
+
 extern bytes magicModuleHeader;
 extern bytes moduleVersion;
 
 class String;
-class Nod;
-class ExpressionNod;
-//class Block;
-//class Block : Node{
-//
-//	Block *begin() const {
-//		return children;  CANT specialize/INHERIT:  obviously
-//	}
-//
-//	Block *end() const{
-//		return children + length;
-//	}
-//};
 
-//char start_function=0x00;//unreachable strange convention
-extern char unreachable;//=0x00;//unreachable strange convention
-//Code &emit(Node root_ast);
-Code &emit(Node root_ast, Module* runtime0=0, String _start="main");
 class Signature;
 
-extern Map<String, Valtype> return_types;
+extern Map<String, Valtype> return_types;// redundant with functionSignatures todo: remove
+extern Map<String, Signature> functionSignatures;// for funcs AND imports, serialized differently (inline for imports and extra functype section)
 extern Map<String /*function*/, List<String> /* implicit indices 0,1,2,… */> locals; // access from Angle!
-extern Map<String, Signature> functionSignatures;
+
+Code &emit(Node root_ast, Module* runtime0=0, String _start="main");
 
