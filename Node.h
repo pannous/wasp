cd28136d7608d36cab4b3237eef260bf097ba728
Pannous <info@pannous.net>
2020-12-10 22:01:17 +0100

    instable wasm tests: testTruthiness() fails, if changed a PREVIOUS test fails. how? memory layout!?

--- Node.h
+++ Node.h
@@ -108,7 +108,7 @@ public:
 //	}
 
 	void *operator new(size_t size) {
-		return static_cast<Node *>(calloc(sizeof(Node), size));// WOW THAT WORKS!!!
+		return (Node *)(calloc(sizeof(Node), size));// WOW THAT WORKS!!!
 	}
 
 	void operator delete(void *a) {
