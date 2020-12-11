747d374d54bd4c92275f7d81dcb0149d813a816c
Pannous <info@pannous.net>
2020-12-11 15:10:54 +0100

    before children->child

--- String.cpp
+++ String.cpp
@@ -430,11 +430,13 @@ void log(chars s) {
 
 bool String::empty() const {//this==0 in testMarkMulti!
 #ifdef WASM
-	if((long) data > memory_size/*bug!*/)
+	if(memory_size>0 and (long) data >= memory_size/*bug!*/){
 		error("CORRUPT String pointer");
-//		return true;
+		logi((long) data);
+		return true;
+	}
 #endif
-	return this == 0 || length == 0 || !data || (long) data < heap_offset/14  || data[0] == 0 || data == object_name.data;
+	return this == 0 || length == 0 || !data || (long) data <=0   || data[0] == 0 || data == object_name.data;
 //		|| data=="" || data=="ø" || data=="[…]"  || data=="(…)"  || data=="{…}"  TODO
 }
 
