8954926424f286d58e1561df7716f0b97291c671
Pannous <info@pannous..com>
2020-12-11 15:56:25 +0100

    wasp testNodeBasic still fails

---
+++ playground/build-wasm.sh
@@ -0,0 +1,59 @@
+echo check leaks
+# valgrind --leak-check=full --track-origins=yes ./mark
+
+echo build wasm
+
+# WASM env:
+# export PATH="/usr/local/opt/llvm/bin:$PATH"
+# export LDFLAGS="-L/usr/local/opt/llvm/lib"
+# export CPPFLAGS="-I/usr/local/opt/llvm/include"
+#export C_INCLUDE_PATH="/usr/include/:/usr/lib/x86_64-linux-gnu/glib-2.0/include/"
+#for x in `ls -d /usr/include/*/`;do export C_INCLUDE_PATH="$C_INCLUDE_PATH:$x";done
+clang=/opt/wasm/wasi-sdk-11.0/bin/clang
+export BINARYEN=/opt/wasm/binaryen/
+export CPATH=/opt/wasm/swift-wasm/usr/share/wasi-sysroot/include/
+
+# clang-10: error: cannot specify -o when generating multiple output files
+#"String.cpp" "Node.cpp" "WasmHelpers.cpp"
+
+# -Os -Oz ok? -O1 removes symbols!
+clang_options="-g  --target=wasm32-unknown-unknown--wasm -nostartfiles -nostdlib -Wl,--demangle,--no-entry,--export-all,--export-table,--allow-undefined"
+clang_options="$clang_options -fdebug-pass-arguments -fdebug-pass-structure "
+
+# --target=wasm32-unknown-emscripten-wasm
+# -Wl…,… == linker flags for wasm-ld
+# --relocatable
+# --print-gc-sections
+# --demangle
+# --export-all
+  # --mllvm <value>        Options to pass to LLVM
+  #   --import-memory        Import memory from the environment
+  # --import-table         Import function table from the environment
+  # --initial-memory=<value>                         Initial size of the linear memory
+  #   --features=<value>
+
+# 
+$clang $clang_options t.c -o t.wasm || exit
+
+# DEBUG:
+llvm-dwarfdump t.wasm > t.dwarf
+# --split-dwarf-file=t.dwarf --split-dwarf-output=t.map ONLY with ELF
+#"--export-all  --flavor wasm
+
+wasm2wat t.wasm > t.auto.wat
+wasm-dis t.wasm > t.wast # wasm2wast
+llvm-objdump -h t.wasm > t.llvm.objdump
+llvm-dwarfdump t.wasm --all -o t.dwarfdump
+wasm-objdump -hxd t.wasm > t.objdump
+wasm-dis -sm t.wasm.map t.wasm -o t.dis
+wasm-decompile t.wasm -o t.decompile # WORKS EVEN IF wasm2wat FAILS!! also very readable
+# No wasm-compile exists: The format is very low-level, much like Wasm itself, so even though it looks more high level than the .wat format, it wouldn't be any more suitable for general purpose programming.
+
+
+#  NO LONGER NECESSARY: clang -g sould work
+/opt/wasm/emscripten/tools/wasm-sourcemap.py t.wasm --dwarfdump-output=t.dwarf -o t.map
+# ./wasm-sourcemap.py -o t.map t.wasm  
+# lldb -- wasmtime -g t.wasm
+
+wasmx t.wasm  # danger, -O0 fucks up memoryBase !
+
