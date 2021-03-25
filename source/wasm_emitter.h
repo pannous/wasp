108d7f121830fd30811ac8fd518d1efaf7e826cf
Pannous <info@pannous.net>
2021-03-25 19:36:22 +0100

    variables! 	assert_emit("i=123;i+1", 124);

--- source/wasm_emitter.h
+++ source/wasm_emitter.h
@@ -19,7 +19,22 @@ class Signature;
 //extern Map<String, Valtype> return_types;// redundant with functionSignatures todo: remove
 extern Map<String, Signature> functionSignatures;// for funcs AND imports, serialized differently (inline for imports and extra functype section)
 extern Map<String, int> functionIndices;
-extern Map<String /*function*/, List<String> /* implicit indices 0,1,2,… */> locals; // access from Angle!
+//extern Map<String /*function*/, List<String> /* implicit indices 0,1,2,… */> locals; // access from Angle!
+//extern Map<String /* implicit indices 0,1,2,… */, Node* /* compile-time modifiers/values? */> globals; // access from Angle!
+
 
 Code &emit(Node root_ast, Module *runtime0 = 0, String _start = "main");
 
+// private, could be moved:
+Code emitWhile(Node &node, String context);
+
+Code emitIf(Node &node, String context);
+
+Code emitCall(Node &node, String context);
+
+Code emitDeclaration(Node fun, Node &body);
+
+Code emitSetter(Node node, Node &value, String context);
+
+//Code emitExpression(Node *nodes, String context);
+Code emitExpression(Node &node, String context/*="main"*/);
