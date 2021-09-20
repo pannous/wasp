d3f37be924956730a53f78b616180a312b991c0c
Pannous <info@pannous.net>
2021-09-20 13:46:13 +0200

    forced sync run_wasm WORKS

--- source/WebView.cpp
+++ source/WebView.cpp
@@ -4,6 +4,73 @@
 // 104234 bytes if compiled with -Oz
 // todo: remove std::string from webview.h for much smaller size?
 
+#include <condition_variable>
+#include <thread>
+#include <chrono>
+//#include<pthread.h> c way?
+//pthread_t wait_for_wasm[2];
+
+std::condition_variable wasm_condition;
+//https://en.cppreference.com/w/cpp/thread/condition_variable
+std::mutex wasm_result_mutex;
+//	https://www.cplusplus.com/reference/mutex/mutex/
+// Even if the shared variable is atomic, it must be modified under the mutex
+
+#include <stdatomic.h>
+
+int wasm_error_code = -1;
+
+class Wait {
+public:
+	void done(int result) {
+		pthread_mutex_lock(&m_mutex);
+		m_done = true;
+		m_result = result;
+		pthread_cond_broadcast(&m_cond);
+		pthread_mutex_unlock(&m_mutex);
+	}
+
+	void wait() {
+		pthread_mutex_lock(&m_mutex);
+		while (!m_done) pthread_cond_wait(&m_cond, &m_mutex);
+		pthread_mutex_unlock(&m_mutex);
+	}
+
+	int result() {
+		int r;
+		pthread_mutex_lock(&m_mutex);
+		r = m_result;
+		pthread_mutex_unlock(&m_mutex);
+		return r;
+	}
+
+private:
+	atomic_int m_result = -3;
+	pthread_mutex_t m_mutex;
+	pthread_cond_t m_cond;
+	bool m_done;
+};
+
+static Wait waiter;
+
+void wasm_done(int wasm_result0) {
+	waiter.done(wasm_result0);
+	{
+		std::lock_guard<std::mutex> lk(wasm_result_mutex);
+		wasm_error_code = 0;
+	}
+//	std::cerr << "Notifying ...\n";
+	wasm_condition.notify_all();//
+}
+
+void wasm_error(std::string s) {
+	waiter.done(-2);
+	{
+		std::lock_guard<std::mutex> lk(wasm_result_mutex);
+		wasm_error_code = -1;
+	}
+}
+
 webview::webview w(true, nullptr);// global for lambdas
 std::string testWebview(std::string s);
 
@@ -65,6 +132,19 @@ int init_graphics() {
 		w.terminate();
 		return s;
 	});
+
+	w.bind("wasm_done", [](std::string s) -> std::string {
+		const std::string &string = webview::json_parse(s, "", 0);
+		int result0 = std::stoi(string);
+		printf("wasm_done  result = %d \n", result0);
+		wasm_done(result0);
+		return s;
+	});
+	w.bind("wasm_error", [](std::string s) -> std::string {
+		printf("wasm_error %s \n", s.data());
+		wasm_error(webview::json_parse(s, "", 0));
+		return s;
+	});
 	w.bind("terminate", [](std::string s) -> std::string {
 		w.terminate();
 		return s;
@@ -88,6 +168,8 @@ int init_graphics() {
 		return s;
 	});// works, with
 	w.bind("alert", [](std::string s) -> std::string {
+		if (not s.find(",")) // keep [x,y]
+			s = webview::json_parse(s, "", 0);// [x] => x
 		w.set_title(s);
 		std::cout << s << std::endl;
 		return s;
@@ -133,33 +215,17 @@ void requestAnimationFrame() {
 	// copy wasp data to canvas
 }
 
+void run_wasm_async(unsigned char *bytes, int length) {
+	std::stringstream ss;
+	ss << "code=new Uint8Array([";
+	for (int i = 0; i < length; i++) ss << ((short) bytes[i]) << ",";
+	ss << "]);wasmx(code);";
+	w.eval(ss.str());
+}
 
-unsigned char test_prog_wasm[] = {
-		0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x15, 0x04, 0x60,
-		0x02, 0x7f, 0x7f, 0x01, 0x7f, 0x60, 0x00, 0x00, 0x60, 0x03, 0x7f, 0x7f,
-		0x7f, 0x01, 0x7f, 0x60, 0x00, 0x01, 0x7e, 0x02, 0x1c, 0x02, 0x03, 0x65,
-		0x6e, 0x76, 0x0a, 0x65, 0x78, 0x74, 0x5f, 0x6d, 0x65, 0x6d, 0x63, 0x70,
-		0x79, 0x00, 0x02, 0x03, 0x65, 0x6e, 0x76, 0x03, 0x73, 0x75, 0x6d, 0x00,
-		0x00, 0x03, 0x04, 0x03, 0x01, 0x03, 0x00, 0x05, 0x06, 0x01, 0x01, 0x80,
-		0x02, 0x80, 0x02, 0x06, 0x09, 0x01, 0x7f, 0x01, 0x41, 0x80, 0x8c, 0xc0,
-		0x02, 0x0b, 0x07, 0x28, 0x04, 0x06, 0x6d, 0x65, 0x6d, 0x6f, 0x72, 0x79,
-		0x02, 0x00, 0x04, 0x74, 0x65, 0x73, 0x74, 0x00, 0x04, 0x0b, 0x74, 0x65,
-		0x73, 0x74, 0x5f, 0x6d, 0x65, 0x6d, 0x63, 0x70, 0x79, 0x00, 0x03, 0x06,
-		0x5f, 0x73, 0x74, 0x61, 0x72, 0x74, 0x00, 0x02, 0x0a, 0x71, 0x03, 0x03,
-		0x00, 0x01, 0x0b, 0x59, 0x02, 0x01, 0x7f, 0x01, 0x7e, 0x23, 0x00, 0x41,
-		0x10, 0x6b, 0x22, 0x00, 0x24, 0x00, 0x20, 0x00, 0x42, 0x00, 0x37, 0x03,
-		0x08, 0x20, 0x00, 0x41, 0xe7, 0x8a, 0x8d, 0x09, 0x36, 0x02, 0x04, 0x20,
-		0x00, 0x41, 0xef, 0x9b, 0xaf, 0xcd, 0x78, 0x36, 0x02, 0x00, 0x20, 0x00,
-		0x41, 0x08, 0x6a, 0x20, 0x00, 0x41, 0x04, 0x6a, 0x41, 0x04, 0x10, 0x00,
-		0x1a, 0x20, 0x00, 0x41, 0x08, 0x6a, 0x41, 0x04, 0x72, 0x20, 0x00, 0x41,
-		0x04, 0x10, 0x00, 0x1a, 0x20, 0x00, 0x29, 0x03, 0x08, 0x21, 0x01, 0x20,
-		0x00, 0x41, 0x10, 0x6a, 0x24, 0x00, 0x20, 0x01, 0x0b, 0x11, 0x00, 0x20,
-		0x00, 0x20, 0x01, 0x6a, 0x20, 0x00, 0x20, 0x01, 0x6b, 0x10, 0x01, 0x41,
-		0x02, 0x6d, 0x0b, 0x0b, 0x0a, 0x01, 0x00, 0x41, 0x80, 0x0c, 0x0b, 0x03,
-		0xa0, 0x06, 0x50
-};
 
-int run_wasm(unsigned char *bytes, int length) {
+// forced synchronous
+int run_wasm_sync(unsigned char *bytes, int length) {
 	// 1. save to APPDATA folder and then fetch via js
 	// NOPE "fetch api cannot load file" could bind my own fetch though!
 
@@ -172,16 +238,32 @@ int run_wasm(unsigned char *bytes, int length) {
 	ss << "]);wasmx(code);";
 	std::string wasm = ss.str();
 	w.eval(wasm);
-//	printf("%s", wasm.data());
-
+//	sleep(2);// lets the sub-thread wait but returns immediately WTF!?! I see why people hate c
+//	waiter.wait();
+	{
+		std::unique_lock<std::mutex> lk(wasm_result_mutex);
+		std::cerr << "Waiting... \n";
+//		wasm_condition.wait(lk, [] { return wasm_error_code >= 0; });
+		wasm_condition.wait(lk);
+		std::cerr << "...finished waiting. result = " << waiter.result();
+//		printf("finished Waiting  result = %d \n", wasm_result);
+	}
 	// 3. feed natively how? BBQ OMG JIT wasm LLInt (low level interpreter)
 	// irrelevant / unprofessional? https://www.youtube.com/watch?v=1v4wPoMskfo
 	// https://webkit.org/blog/9329
-	return -42;// cant wait!
+	return waiter.result();// cant wait!
+}
+
+
+int run_wasm(unsigned char *bytes, int length) {
+	run_wasm_sync(bytes, length);
+	return waiter.result();
 }
 
 std::string testWebview(std::string s) {
-	testCurrent();
+	std::thread teste(testCurrent);
+	teste.detach();
+//	testCurrent();
 	return s;
 }
 
