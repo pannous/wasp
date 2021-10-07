b8641eab5c5bad66b94d09db0c563ea34d42bb42
Pannous <info@pannous..com>
2021-10-07 23:22:54 +0200

    ALL TESTS PASSED BUT	assert_emit("‖-3‖+1", 4); never worked!?

--- source/Util.cpp
+++ source/Util.cpp
@@ -16,25 +16,17 @@ bool fileExists(String filename) {
 
 String findFile(String filename) {
 	if (filename.empty())return "";
-	//		const char *homedir = getpwuid(getuid())->pw_dir;
-	//		filename = filename.replace("~",  homedir);
+//	filename = filename.replace("~", getpwuid(getuid())->pw_dir /*homedir*/);
 	if (fileExists(filename))return filename;
-	if (fileExists(filename + ".wasm"))return filename + ".wasm"s;
-	if (fileExists(filename + ".wast"s))return filename + ".wast"s;
+	// todo: check wasm date, recompile if older than wasp / wast
 	if (fileExists(filename + ".wasp"))return filename + ".wasp"s;
+	if (fileExists(filename + ".wast"s))return filename + ".wast"s;
+	if (fileExists(filename + ".wasm"))return filename + ".wasm"s;
 	//		if (not filename.contains("/"))filename = findFile(project + "/" + filename) || filename;
 	if (not filename.contains("/"))filename = findFile("lib/"s + filename) || filename;
 	if (not filename.contains("/"))filename = findFile("src/"s + filename) || filename;
 	if (not filename.contains("/"))filename = findFile("source/"s + filename) || filename;
 	if (not filename.contains("/"))filename = findFile("include/"s + filename) || filename;
 	if (not filename.contains("/"))filename = findFile("samples/"s + filename) || filename;
-//#if DEBUG
-	char cwd[1024];
-	getcwd(cwd, sizeof(cwd));
-	log(cwd);
-	if (not fileExists(filename) and not filename.contains("/"))
-		if (contains(cwd, "cmake-build"))
-			return findFile("../"s + filename);// todo DANGER don't use outside cmake-build-default tests!
-//#endif
 	return fileExists(filename) ? filename : "";
 }
