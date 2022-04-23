021d7ba53572e40b15831b62a2d8ec094939c2c0
Pannous <info@pannous..com>
2022-04-23 11:00:59 +0200

    added console

---
+++ source/console.cpp
@@ -0,0 +1,75 @@
+//#include <stdio.h>
+//#include <unistd.h>
+//#include <stdlib.h>
+//#include <ctype.h>
+//#include <termios.h>
+#include <string.h>
+//#include "String.h"
+//#include <vector>
+//#include <string>
+
+#include "Wasp.h"
+
+//#include "util.hpp"
+//#ifndef NO_READLINE
+#define USE_READLINE
+
+#include <readline/history.h> // libreadline-dev
+#include <readline/readline.h>
+//#endif
+
+//using namespace std;
+// static struct termios stored_settings;
+
+void showHelpMessage() {
+	printf("⚠️ The wasp console currently works by re-executing after each new entry. Be careful with destructive or external effects!");
+//	ps("AVAILABLE COMMANDS:");
+//	ps("help :h or ?");
+}
+
+bool file_read_done = false;
+
+void getline(char *buf) {
+	if (buf == 0) return; // end
+	int MAXLENGTH = 10000;
+	const char *PROMPT = "wasp> ";
+#ifdef USE_READLINE
+	if (!file_read_done) file_read_done = 1 + read_history(".wasp_history");
+	char *tmp = readline(PROMPT);
+	if (tmp == 0 or strlen(tmp) == 0) {
+		return;
+	}
+//	tmp=fixQuotesAndTrim(tmp);// LATER!
+	if (strncmp(tmp, buf, MAXLENGTH) and strlen(tmp) > 0) add_history(tmp); // only add new content
+	strncpy(buf, tmp, MAXLENGTH);
+	buf[MAXLENGTH - 1] = '\0';
+	write_history(".wasp_history");
+#else
+
+#endif
+}
+
+//bool parse(string* data) {
+static char *lastCommand;
+static char *commandCode;
+
+char *version = "1.0";
+
+[[noreturn]] void console() {
+	printf("\nWasp version %s\n", version);
+
+	char *data = (char *) malloc(10000);
+#ifdef signal
+	setjmp(try_context); //recovery point
+#endif
+	debug = false;
+	String code;
+	while (true) {
+		getline(data);
+		code += data;
+		Node &result = parse(code);// safeMode only for web access
+		printf(">>> ");
+//	result.interpret().print();
+//		result.eval().print();
+	}
+}
