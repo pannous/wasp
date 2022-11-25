151de0cd56a9d08b040962494c84f1bdfb1f55a1
Pannous <info@pannous..com>
2022-11-25 13:18:58 +0100

    before Function.locals

--- source/WitReader.h
+++ source/WitReader.h
@@ -59,7 +59,7 @@ class WitReader {
         return *pNode;
     }
 
-    Node &readType(Node node) {
+    Node &readType(Node &node) {
         Node &alias = node[1];
         Node &type = analyzeType(node.value.node);
         if (types.has(type.name))
@@ -86,7 +86,7 @@ class WitReader {
 
 
     // todo resource interface variant are currently handled IDENTICALLY!
-    void readResource(Node node) {
+    Node &readResource(Node &node) {
         Node &resource = node[1];
         trace("\nwit resource:");
         trace(resource);
@@ -100,15 +100,15 @@ class WitReader {
         else if (types[resource.name] != resource)
             error("resource already known:\n"s + resource.serialize());
         else warn(("resource already known: "s + resource.name));
+        return resource;
         // todo: module namespacing ,
         //  fuzzing?
     }
 
-    void readInterface(Node node) {
+    Node &readInterface(Node &node) {
         Node &interface = node[1];
         if (interface.name == "func") {
-            readFunc(interface);
-            return;
+            return readFunc(interface);
         }
         for (auto field: interface) {
             Node &member = analyzeWit(field);
@@ -127,6 +127,7 @@ class WitReader {
 
         trace("\nwit interface:");
         trace(interface);
+        return interface;
     }
 
     Node &readEnum(Node &node, Kind kind) {
@@ -159,26 +160,26 @@ class WitReader {
         return enuma;
     }
 
-    void readVariant(Node node) {
+    Node &readVariant(Node &node) {
         Node &variant = readEnum(node, variants);
         trace("\nwit variant:");
         trace(variant);
+        return variant;
     }
 
 
-    void readRecord(Node node) {
+    Node &readRecord(Node &node) {
         Node &record = node[1];
         trace("\nwit record:");
         trace(record);
+        return record;
     }
 
-    void readFlags(Node node) {
-        readEnum(node, flags);
-        trace("\nwit flags:");
-        trace(flags);
+    Node &readFlags(Node &node) {
+        return readEnum(node, flags);
     }
 
-    Node &readFunc(Node node) {
+    Node &readFunc(Node &node) {
         String &name = node.name;
         if (name == "static")
             name = node[1].name;
@@ -193,14 +194,15 @@ class WitReader {
         return *func;
     }
 
-    void readModule(Node node) {
-        Node module = node[2];// node[1] == "$"
+    Node &readModule(Node &node) {
+        Node &module = node[2];// node[1] == "$"
         trace("\nwit module:");
         trace(module);
+        return module;
     }
 
     // outdated (@interface … ) format
-    void readInterfaceFunc(Node &node) {
+    Node &readInterfaceFunc(Node &node) {
         Node &func = node[0][2];
         Function &fun = *new Function();
         if (func[0].name == "export")
@@ -211,72 +213,7 @@ class WitReader {
 //            Node &arg = node[i];
 //            if(arg.name)
         }
-
-    }
-
-    Node &analyzeWit(Node &node) {
-
-        if (node.first().name == "func")
-            return readFunc(node);
-
-        bool keys = node.kind == key or (node.kind == groups and node.length == 2);
-        keys = keys or (node.length == 3 and node[2].name == "interface");
-        for (auto n: node) {
-            if (keys)n = node;
-            if (n.kind == key)
-                n.add(n.value.node);
-            String &entry = n.first().name;
-            if (entry == "@witx") {
-                node.remove(0, 0);
-                continue;
-            }
-            if (entry == "static")
-                if (n.length == 0)continue;
-                else readFunc(n);
-            else if (entry == "type") {
-                readType(n);
-            } else if (entry == "resource") {
-                readResource(n);
-            } else if (entry == "variant") {
-                readVariant(n);
-            } else if (entry == "enum") {
-                readEnum(n, enums);
-            } else if (entry == "option") {
-//                todo("ignore, since all nodes are optionals by default unless marked as required!")
-            } else if (entry == "@interface") {
-                readInterfaceFunc(node);
-            } else if (entry == "interface") {
-                readInterface(n);
-            } else if (entry == "record") {
-                readRecord(n);
-            } else if (entry == "flags") {
-                readFlags(n);
-            } else if (entry == "func") {
-                readFunc(n);
-            } else if (entry == "@interface") {
-                if (node.length > 1 and node[1] == "func")
-                    readFunc(node);
-                else warn("Old @interface syntax not supported");
-            } else if (n.kind == key and n.value.node->name == "func") {
-                readFunc(n);
-            } else if (n.name == "module") {
-                readModule(node);
-                break;
-            } else if (entry == "module" or entry == "world" or entry == "resource") {// we don't distinguish yet!
-                n.add(node[1]);// hack
-                readModule(n);
-                break;
-            } else if (n.name == "%") {
-                warn("wit % nonsense "s + node.serialize());
-                break;
-            } else if (n.kind == groups) {
-                for (auto c: node)
-                    analyzeWit(c);
-            } else
-                error("UNKNOWN wit entry: "s + node.serialize());
-            if (keys)break;
-        }
-        return node;
+        return func;
     }
 
 public:
@@ -295,6 +232,78 @@ public:
         Node &node = parse(file.data, options);
         return analyzeWit(node);
     }
+
+    Node &analyzeWit(Node &node) {
+        if (node.first().name == "func")
+            return readFunc(node);
+
+        bool keys = node.kind == key or (node.kind == groups and node.length == 2);
+        keys = keys or (node.length == 3 and node[2].name == "interface");
+        for (Node &n: node) {
+            if (keys)n = node;
+            if (n.kind == key)
+                n.add(n.value.node);
+            String &entry = n.first().name;
+            if (entry == "@witx") {
+                node.remove(0, 0);
+                continue;
+            } else if (entry == "module" or entry == "world" or entry == "resource") {// we don't distinguish yet!
+                n.add(node[1]);// hack
+                readModule(n);
+                break;
+            } else if (n.name == "module") {
+                readModule(node);
+                break;
+            } else if (entry == "@interface") {
+                if (node.length > 1 and node[1] == "func")
+                    readFunc(node);
+                else warn("Old @interface syntax not supported");
+            } else if (entry == "@interface") {
+                return readInterfaceFunc(node);
+            } else
+                n = analyzeWitEntry(n);
+        }
+        return node;
+    }
+
+    Node &analyzeWitEntry(Node &n) {
+        String &entry = n.first().name;
+        if (entry == "static")
+            if (n.length == 0)return NUL;
+            else return readFunc(n);
+        else if (entry == "type") {
+            return readType(n);
+        } else if (entry == "resource") {
+            return readResource(n);
+        } else if (entry == "variant") {
+            return readVariant(n);
+        } else if (entry == "flags") {
+            return readFlags(n);
+        } else if (entry == "enum") {
+            return readEnum(n, enums);
+        } else if (entry == "option") {
+//                todo("ignore, since all nodes are optionals by default unless marked as required!")
+        } else if (entry == "module") {
+            return readModule(n);
+        } else if (entry == "interface") {
+            return readInterface(n);
+        } else if (entry == "record") {
+            return readRecord(n);
+        } else if (entry == "func") {
+            return readFunc(n);
+        } else if (n.kind == key and n.value.node->name == "func") {
+            return readFunc(n);
+        } else if (n.name == "%") {
+            warn("wit % nonsense "s + n.serialize());
+        } else if (n.kind == groups) {
+            for (Node &c: n)
+                c = analyzeWit(c);
+            return n;
+        } else
+            error("UNKNOWN wit entry: "s + n.serialize());
+        return NUL;
+    }
+
 };
 
 
