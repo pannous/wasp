0b7547a16f1c574ba453e4929afe9891a4fe0bea
Pannous <info@pannous..com>
2022-11-26 19:49:58 +0100

    ALL TESTS PASSING including runtime_emit's ! 15 sec

--- source/tests.cpp
+++ source/tests.cpp
@@ -2515,7 +2515,7 @@ void tests() {
 // 2021-10 : 40 sec for Wasm3
 // 2021-10 : 10 sec in Webapp / wasmtime
 // 2022-05 : 8 sec in Webapp / wasmtime with wasp.wasm build via wasm-runtime
-// 2022-10 : 3 sec without runtime_emit
+// 2022-10-26 : 3 sec without runtime_emit, 15  sec with runtime_emit  ALL TESTS PASSING
 void testCurrent() {
     //	throwing = false;// shorter stack trace
     //	panicking = true;//
