1b68ffdf84ee57046e1db98d71207c607ee5a766
Pannous <info@pannous.com>
2022-12-28 01:26:16 +0100

    CURRENT TESTS PASSED including testIf which was lost in tests before Process finished with exit code 42

--- source/test_angle.cpp
+++ source/test_angle.cpp
@@ -37,53 +37,46 @@ void testCall() {
     assert_is("1 + square 1+2", 10)
     skip(
     // interpreter broken lol
-			assert_is("1+square(2+3)", 26)
-			assert_is("square{i:3}", 9) //todo: match arguments!
-	)
+            assert_is("1+square(2+3)", 26)
+            assert_is("square{i:3}", 9) //todo: match arguments!
+    )
 }
 
 void testIf() {
-	assert_eval("if(2):{3}", 3);
-	assert_eval("if 2 : 3 else 4", 3);
+    skip( // todo:
+            assert_eval("if '':3", false);
+            assert_eval("if ():3", false);
+            assert_eval("if ø:3", false);
+            assert_eval("if {}:3", false);
+            assert_eval("if x:3", false);
+    )
 
-	assert_eval("if 0:3", false);
-	skip(
-	    assert_eval("if(0):{3}", false);
-			assert_eval("if '':3", false);
-			assert_eval("if ():3", false);
-			assert_eval("if ø:3", false);
-			assert_eval("if {}:3", false);
-			assert_eval("if x:3", false);
-	)
+    assert_eval("if(2):{3}", 3);
+    assert_eval("if 2 : 3 else 4", 3);
 
-	skip(// esotheric shit
-			assert_eval("if(0,then=3,else=4)", 4);
-			assert_eval("if(1,then=3,else=4)", 3);
-			assert_eval("if(2,then=3)", 3);
-			assert_eval("if(condition=2,then=3)", 3);
-			assert_eval("if(condition=0,then=3,else=4)", 4);
-			assert_eval("if(condition=1,then=3,else=4)", 3);
-	)
-	assert_eval("if(0):{3} else {4}", 4);
-	assert_eval("if 0:{3} else {4}", 4);
-	assert_eval("if 0:3 else {4}", 4);
-	assert_eval("if 0 {3} else {4}", 4);
-	assert_eval("if {0}:3 else 4", 4);
-	assert_eval("if 0:3 else 4", 4);
-	assert_eval("if 0:{3} else 4", 4);
-	assert_eval("if (2) {3} else 4", 3);
-	assert_eval("if(2){3} else 4", 3);
-	assert_eval("if(0){3} else 4", 4);
-	assert_eval("if 2 {3} else {4}", 3);
-	assert_eval("if 0 {3} else {4}", 4);
-	assert_eval("if (2) {3} else {4}", 3);
-	assert_eval("if(2){3} else {4}", 3);
-	assert_eval("if(0){3} else {4}", 4);
-	assert_eval("if (0) {3} else {4}", 4);
-	assert_eval("if(2):{3} else 4", 3);
-	assert_eval("if(2):{3} else {4}", 3);
-	assert_eval("if 2:{3} else 4", 3);
-	assert_eval("if 2:3 else 4", 3);
+    assert_eval("if 0:3", false);
+    assert_eval("if(0):{3}", false);
+
+    assert_eval("if(0):{3} else {4}", 4);
+    assert_eval("if 0:{3} else {4}", 4);
+    assert_eval("if 0:3 else {4}", 4);
+    assert_eval("if 0 {3} else {4}", 4);
+    assert_eval("if {0}:3 else 4", 4);
+    assert_eval("if 0:3 else 4", 4);
+    assert_eval("if 0:{3} else 4", 4);
+    assert_eval("if (2) {3} else 4", 3);
+    assert_eval("if(2){3} else 4", 3);
+    assert_eval("if(0){3} else 4", 4);
+    assert_eval("if 2 {3} else {4}", 3);
+    assert_eval("if 0 {3} else {4}", 4);
+    assert_eval("if (2) {3} else {4}", 3);
+    assert_eval("if(2){3} else {4}", 3);
+    assert_eval("if(0){3} else {4}", 4);
+    assert_eval("if (0) {3} else {4}", 4);
+    assert_eval("if(2):{3} else 4", 3);
+    assert_eval("if(2):{3} else {4}", 3);
+    assert_eval("if 2:{3} else 4", 3);
+    assert_eval("if 2:3 else 4", 3);
     assert_eval("if 2:{3} else {4}", 3);
     assert_eval("if 2:3 else {4}", 3);
     assert_eval("if 2 {3}", 3);
@@ -93,8 +86,9 @@ void testIf() {
     assert_eval("if (0) {3} else 4", 4);
 //	assert_eval("2 then 3 else 4", 3);
     skip(
-    assert_eval("2 and 3 or 4", 3);
-            )
+            assert_eval("2 and 3 or 4", 3);
+            assert_eval("false else 3", 3);
+    )
     assert_eval("1 and 0 or 4", 4);
     assert_eval("if 1 then 0 else 4", (int64) 0);
     assert_eval("if 0:{3} else 4", 4);
@@ -102,68 +96,74 @@ void testIf() {
 
     assert_eval("if 0 {3}", false);
     assert_eval("false or 3", 3);
-    assert_eval("false else 3", 3);
-    assert_eval("4 or 3", 4);
+//    assert_eval("4 or 3", 4);
+    assert_eval("4 or 3", 7);
 //	assert_eval("4 else 3", 4);
     assert_eval("if (2) {3}", 3);
     assert_eval("if(2){3}", 3);
-	assert_eval("if(0){3}", false);
-	assert_eval("if 2:{3}", 3);
-	assert_eval("if 2:3", 3);
+    assert_eval("if(0){3}", false);
+    assert_eval("if 2:{3}", 3);
+    assert_eval("if 2:3", 3);
 
-	assert_eval("if 0 {3} else 4", 4);
-	assert_eval("if 2 {3} else 4", 3);
+    assert_eval("if 0 {3} else 4", 4);
+    assert_eval("if 2 {3} else 4", 3);
 
-//	assert_eval("if 2 , 3 , 4", 3);
-//	assert_eval("if{2 , 3 , 4}", 3);
-//	assert_eval("if 2 then 3 else 4", 3);
-	assert_eval("if(2,3,4)", 3);
-	assert_eval("if({2},{3},{4})", 3);
-	assert_eval("if(2){3}{4}", 3);
-	skip(
-			assert_parses("if(condition=2,then=3,else=4)");
-			check(result["condition"] == 2);
-			check(result["else"] == 4);
-			assert_eval("if(condition=2,then=3,else=4)", 3); // this is what happens under the hood (?)
-	)
+    assert_eval("if{2 , 3 , 4}", 3);
+    assert_eval("if 2 then 3 else 4", 3);
+    assert_eval("if(2,3,4)", 3);
+    assert_eval("if({2},{3},{4})", 3);
+    skip(// esotheric nonsense?
+            assert_eval("if 2 , 3 , 4", 3);
+            assert_eval("if(2){3}{4}", 3); // maybe todo?
+            assert_eval("if(0,then=3,else=4)", 4);
+            assert_eval("if(1,then=3,else=4)", 3);
+            assert_eval("if(2,then=3)", 3);
+            assert_eval("if(condition=2,then=3)", 3);
+            assert_eval("if(condition=0,then=3,else=4)", 4);
+            assert_eval("if(condition=1,then=3,else=4)", 3);
+            assert_parses("if(condition=2,then=3,else=4)");
+            check(result["condition"] == 2);
+            check(result["else"] == 4);
+            assert_eval("if(condition=2,then=3,else=4)", 3); // this is what happens under the hood (?)
+    )
 }
 
 
 void testIfMath() {
-	assert_eval("if 0+2:{3*1} else 4+0", 3);
+    assert_eval("if 0+2:{3*1} else 4+0", 3);
 
-	skip( // no colon => no work. ok!
-			assert_eval("if 2+0 {3*1} else {4*1}", 3);
-			assert_eval("if 2+0 {3*1}", 3);
+    skip( // no colon => no work. ok!
+            assert_eval("if 2+0 {3*1} else {4*1}", 3);
+            assert_eval("if 2+0 {3*1}", 3);
 
-	)
-	assert_eval("if(0*2):{3*1} else {4*1}", 4);
+    )
+    assert_eval("if(0*2):{3*1} else {4*1}", 4);
 
-	assert_eval("if 2+0 then 3 else 4+0", 3);
+    assert_eval("if 2+0 then 3 else 4+0", 3);
 
 //	assert_group("if 2+0 : 3 else 4+0", "(if (2+0) (3) (4+0))");
-	assert_eval("if 2+0 : 3 else 4+0", 3);
-	assert_eval("if 0*2:{3*1} else {4*1}", 4);
-	assert_is("2+0", 2)
-	assert_eval("if 0*2:3*1", false);
-	skip(
-			assert_eval("if(2,then=3*1)", 3);
-			assert_eval("if(0,then=3,else=4*1)", 4);
-			assert_eval("if(1,then=3+0,else=4)", 3);
-	)
-	assert_eval("if 0*2:3 else {4*1}", 4);
-	assert_eval("if 0*2 {3*1} else {4*1}", 4);
-	assert_eval("if {0}:3 else 4+0", 4);
-	assert_eval("if 0*2:3 else 4+0", 4);
-	assert_eval("if 0*2:{3*1} else 4+0", 4);
-	assert_eval("if(2*1):{3*1}", 3);
-	assert_eval("if (2*1) {3*1} else 4+0", 3);
-	assert_eval("if(2*1){3*1} else 4+0", 3);
-	assert_eval("if(0*2){3*1} else 4+0", 4);
-	assert_eval("if 0*2 {3*1} else {4*1}", 4);
-	assert_eval("if (2*1) {3*1} else {4*1}", 3);
-	assert_eval("if(2*1){3*1} else {4*1}", 3);
-	assert_eval("if(0*2){3*1} else {4*1}", 4);
+    assert_eval("if 2+0 : 3 else 4+0", 3);
+    assert_eval("if 0*2:{3*1} else {4*1}", 4);
+    assert_is("2+0", 2)
+    assert_eval("if 0*2:3*1", false);
+    skip(
+            assert_eval("if(2,then=3*1)", 3);
+            assert_eval("if(0,then=3,else=4*1)", 4);
+            assert_eval("if(1,then=3+0,else=4)", 3);
+    )
+    assert_eval("if 0*2:3 else {4*1}", 4);
+    assert_eval("if 0*2 {3*1} else {4*1}", 4);
+    assert_eval("if {0}:3 else 4+0", 4);
+    assert_eval("if 0*2:3 else 4+0", 4);
+    assert_eval("if 0*2:{3*1} else 4+0", 4);
+    assert_eval("if(2*1):{3*1}", 3);
+    assert_eval("if (2*1) {3*1} else 4+0", 3);
+    assert_eval("if(2*1){3*1} else 4+0", 3);
+    assert_eval("if(0*2){3*1} else 4+0", 4);
+    assert_eval("if 0*2 {3*1} else {4*1}", 4);
+    assert_eval("if (2*1) {3*1} else {4*1}", 3);
+    assert_eval("if(2*1){3*1} else {4*1}", 3);
+    assert_eval("if(0*2){3*1} else {4*1}", 4);
     assert_eval("if (0*2) {3*1} else {4*1}", 4);
     assert_eval("if(2*1):{3*1} else 4+0", 3);
     assert_eval("if(2*1):{3*1} else {4*1}", 3);
@@ -185,52 +185,52 @@ void testIfMath() {
     assert_eval("if 0+2:3*1", 3);
     skip(
             assert_eval("if 0*2 {3*1} else 4+0", 4);
-			assert_eval("if 2+0 {3*1} else 4+0", 3);
-	)
-	assert_eval("if(2,3,4)", 3);
-	assert_eval("if({2},{3*1},{4*1})", 3);
-	assert_eval("if(2*1){3*1}{4*1}", 3);
+            assert_eval("if 2+0 {3*1} else 4+0", 3);
+    )
+    assert_eval("if(2,3,4)", 3);
+    assert_eval("if({2},{3*1},{4*1})", 3);
+    assert_eval("if(2*1){3*1}{4*1}", 3);
 }
 
 
 void testIfGt() {
-	assert_eval("if(2<4):{3}", 3);
-	assert_eval("1<0 or 3", 3);
-	assert_eval("1<0 else 3", 3);
-	assert_eval("4 or 3", 4);
-	assert_eval("if (1<2) {3} else {4}", 3);
-	skip(// maybe later: auto-group:
-			assert_eval("if 1<2 {3} else {4}", 3);
-			assert_eval("if 0>1 {3} else {4}", 4);
-			assert_eval("if (0<1) {3} else {4}", 4);
-			assert_eval("if 1<2 {3}", 3);
-	)
-	assert_eval("if (1<0) {3}", false);
-	assert_eval("if (0<1) {3}", 3);
+    assert_eval("if(2<4):{3}", 3);
+    assert_eval("1<0 or 3", 3);
+    assert_eval("1<0 else 3", 3);
+    assert_eval("4 or 3", 4);
+    assert_eval("if (1<2) {3} else {4}", 3);
+    skip(// maybe later: auto-group:
+            assert_eval("if 1<2 {3} else {4}", 3);
+            assert_eval("if 0>1 {3} else {4}", 4);
+            assert_eval("if (0<1) {3} else {4}", 4);
+            assert_eval("if 1<2 {3}", 3);
+    )
+    assert_eval("if (1<0) {3}", false);
+    assert_eval("if (0<1) {3}", 3);
 
-	assert_eval("if(3<0):{3} else {4}", 4);
-	assert_eval("if 0>1 : {3} else {4}", 4);
-	assert_eval("if 0>1 : 3 else {4}", 4);
-	assert_eval("if 0>1 : 3 else 4", 4);
-	assert_eval("if 0>1:3 else 4", 4);
-	assert_eval("if 0>1:{3} else {4}", 4);
-	assert_eval("if 0>1:3 else {4}", 4);
+    assert_eval("if(3<0):{3} else {4}", 4);
+    assert_eval("if 0>1 : {3} else {4}", 4);
+    assert_eval("if 0>1 : 3 else {4}", 4);
+    assert_eval("if 0>1 : 3 else 4", 4);
+    assert_eval("if 0>1:3 else 4", 4);
+    assert_eval("if 0>1:{3} else {4}", 4);
+    assert_eval("if 0>1:3 else {4}", 4);
 
-	assert_eval("if 0>1 {3} else {4}", 4);
-	assert_eval("if 1<2 : 3 else 4", 3);
+    assert_eval("if 0>1 {3} else {4}", 4);
+    assert_eval("if 1<2 : 3 else 4", 3);
 //	assert_eval("if 3<2 5 else 4", 4);
 
-	assert_eval("if 0>1:3", false);
-	assert_eval("if (2<3) {3} else 4", 3);
-	assert_eval("if(2<4){3} else 4", 3);
-	assert_eval("if(3<0){3} else 4", 4);
+    assert_eval("if 0>1:3", false);
+    assert_eval("if (2<3) {3} else 4", 3);
+    assert_eval("if(2<4){3} else 4", 3);
+    assert_eval("if(3<0){3} else 4", 4);
 
-	assert_eval("if (2<3) {3} else {4}", 3);
-	assert_eval("if(2<4){3} else {4}", 3);
-	assert_eval("if(3<0){3} else {4}", 4);
+    assert_eval("if (2<3) {3} else {4}", 3);
+    assert_eval("if(2<4){3} else {4}", 3);
+    assert_eval("if(3<0){3} else {4}", 4);
 
-	assert_eval("if(2<4):{3} else 4", 3);
-	assert_eval("if(2<4):{3} else {4}", 3);
+    assert_eval("if(2<4):{3} else 4", 3);
+    assert_eval("if(2<4):{3} else {4}", 3);
     assert_eval("if 1<2:{3} else 4", 3);
     assert_eval("if 1<2:3 else 4", 3);
     assert_eval("if 1<2:{3} else {4}", 3);
@@ -253,35 +253,35 @@ void testIfGt() {
     assert_eval("if 1<2:{3}", 3);
     assert_eval("if 1<2:3", 3);
 
-	assert_eval("if 0>1 {3} else 4", 4);
+    assert_eval("if 0>1 {3} else 4", 4);
 
 //	assert_eval("if 1<2 , 3 , 4", 3);
 //	assert_eval("if{2 , 3 , 4}", 3);
 //	assert_eval("if 1<2 then 3 else 4", 3);
-	skip( // esotheric
-			assert_eval("if(2<4,3,4)", 3);
-			assert_eval("if(3<{2},{3},{4})", 3);
-			assert_eval("if(2<4){3}{4}", 3);
-			assert_eval("if 1<2 {3} else 4", 3);
+    skip( // esotheric
+            assert_eval("if(2<4,3,4)", 3);
+            assert_eval("if(3<{2},{3},{4})", 3);
+            assert_eval("if(2<4){3}{4}", 3);
+            assert_eval("if 1<2 {3} else 4", 3);
 
-			assert_parses("if(3<condition=2,then=3,else=4)");
-			check(result["condition"] == 2);
-			check(result["else"] == 4);
-			assert_eval("if(3<condition=2,then=3,else=4)", 3); // this is what happens under the hood (?)
-	)
+            assert_parses("if(3<condition=2,then=3,else=4)");
+            check(result["condition"] == 2);
+            check(result["else"] == 4);
+            assert_eval("if(3<condition=2,then=3,else=4)", 3); // this is what happens under the hood (?)
+    )
 }
 
 
 void testSwitchEvaluation() {
-	assert_is("{a:1+1 b:2}(a)", 2)
-	assert_is("x=a;{a:1 b:2}(x)", 1)
-	// functor switch(x,xs)=xs[x] or xs[default]
+    assert_is("{a:1+1 b:2}(a)", 2)
+    assert_is("x=a;{a:1 b:2}(x)", 1)
+    // functor switch(x,xs)=xs[x] or xs[default]
 }
 
 void testSwitch() {
 //	todo if(1>0) ... innocent groups
-	todo_emit(assert_is("{a:1 b:2}[a]", 1))
-	todo_emit(assert_is("{a:1 b:2}[b]", 2))
+    todo_emit(assert_is("{a:1 b:2}[a]", 1))
+    todo_emit(assert_is("{a:1 b:2}[b]", 2))
 }
 
 /*
@@ -318,45 +318,45 @@ void testSmartTypes(){
 }
 */
 void nl() {
-	put_char('\n');
+    put_char('\n');
 }
 
 //Prescedence typo for Precedence
 void testLogicPrecedence() {
-	check(precedence("and") > 1);
-	check(precedence("and") < precedence("or"));
-	assert_is("true", true);
-	assert_is("false", false);
+    check(precedence("and") > 1);
+    check(precedence("and") < precedence("or"));
+    assert_is("true", true);
+    assert_is("false", false);
     Node ok = parse("true or true");
 //	const Node &ja = ok.interpret();// Undefined symbols if not compiled with Interpret.cpp
 //	print(ja);
-	assert_is("true or true", true);
-	assert_is("true or false", true);
-	assert_is("true and false", false);
-	assert_is("1 ⋁ 1 ∧ 0", 1);
-	assert_is("1 ⋁ 0 ∧ 1", 1);
-	assert_is("1 ⋁ 0 ∧ 0", 1);
-	assert_is("0 ⋁ 1 ∧ 0", 0);
-	assert_is("0 ⋁ 0 ∧ 1", 0);
-	assert_is("true or true and false", true);
-	assert_is("true or false and true", true);
-	assert_is("true or false and false", true);
-	assert_is("false or true and false", false);
-	assert_is("false or false and true", false);
+    assert_is("true or true", true);
+    assert_is("true or false", true);
+    assert_is("true and false", false);
+    assert_is("1 ⋁ 1 ∧ 0", 1);
+    assert_is("1 ⋁ 0 ∧ 1", 1);
+    assert_is("1 ⋁ 0 ∧ 0", 1);
+    assert_is("0 ⋁ 1 ∧ 0", 0);
+    assert_is("0 ⋁ 0 ∧ 1", 0);
+    assert_is("true or true and false", true);
+    assert_is("true or false and true", true);
+    assert_is("true or false and false", true);
+    assert_is("false or true and false", false);
+    assert_is("false or false and true", false);
 }
 
 
 void testAllAngle() {
-	testLogicPrecedence();
+    testLogicPrecedence();
 //	testSmartTypes();
-	testIf();
-	testCall();
-	skip(
-			testSwitch();
-	)
+    testIf();
+    testCall();
+    skip(
+            testSwitch();
+    )
 //	testFunctionParams();
 }
 
 void testAngle() {
-	testAllAngle();
+    testAllAngle();
 }
