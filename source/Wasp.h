158b233b3b75f5c7d848e4ea4c92a9caa1b298c2
Pannous <info@pannous.com>
2022-12-28 20:48:59 +0100

    new node(), parse, debug, serialize in js works

--- source/Wasp.h
+++ source/Wasp.h
@@ -17,6 +17,8 @@ extern codepoint opening_special_brackets[];// circumfixOperators «…»
 extern chars import_keywords[];
 extern List<chars> operator_list;
 
+extern "C" Node *Parse(chars source);
+
 Node &parse(chars source);
 
 Node &parse(String source, ParserOptions parserOptions = {});// wasp -> data
