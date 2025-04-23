f736986eae13ca45b0547ebcff1a04e59a681b4e
pannous <info@pannous.com>
2025-04-23 15:13:14 +0200

    CURRENT TESTS PASSED

--- docs/assets/ln_does_not_persist_need_to_cp.sh
+++ docs/assets/ln_does_not_persist_need_to_cp.sh
@@ -3,3 +3,4 @@ cp ../../cmake-build-wasm-hosted/wasp-hosted.wasm.map .
 cp ../../cmake-build-wasm-debug/wasp.wasm wasp-debug.wasm
 cp ../../cmake-build-wasm-runtime/wasp-runtime.wasm .
 cp ../../cmake-build-wasm-release/wasp.wasm .
+echo "runtime .wasm assets updated"
