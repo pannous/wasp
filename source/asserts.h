01dab2573003aa5f71ff45640753debbcded65d0
pannous <info@pannous.com>
2025-04-29 10:10:47 +0200

    CURRENT TESTS PASSED ALL TESTS PASSED testRun SUCCEEDED

--- source/asserts.h
+++ source/asserts.h
@@ -39,8 +39,6 @@ else{printf("FAILED %s==%s\n",#wasp,#result); \
 backtrace_exit()}
 #endif
 
-#define assert_eval assert_is
-
 #define assert(condition) try{\
 if((condition)==0){printf("\n%s\n",#condition);error("assert FAILED");}else printf("\nassert OK: %s\n",#condition);\
 }catch(chars m){printf("\n%s\n%s",m,#condition);backtrace_exit()}
@@ -48,6 +46,8 @@ if((condition)==0){printf("\n%s\n",#condition);error("assert FAILED");}else prin
 // TODO silent asserts outside of tests!
 #define assert_equals(α, β) if (!assert_equals_x(α,β)){printf("%s != %s",#α,#β);backtrace_exit();}
 
+
+// todo merge with assert_isx
 bool assert_equals_x(String a, String b, chars context = "");
 
 bool assert_equals_x(int64 a, int64 b, chars context = "");
@@ -63,6 +63,7 @@ static List<String> done;
 #elif (MY_WASM or WASM) and not EMSCRIPTEN
 #define assert_emit(α, β) if(!done.has(α)){ done.add(α);assert_expect(new Node(β));eval(α);async_yield();};
 #else
+// #define assert_emit(α, β) printf("%s\n%s:%d\n",α,__FILE__,__LINE__);if (!assert_isx(eval(α),β)){printf("%s != %s",#α,#β);backtrace_exit();}
 #define assert_emit(α, β) printf("%s\n%s:%d\n",α,__FILE__,__LINE__);if (!assert_equals_x(eval(α),β)){printf("%s != %s",#α,#β);backtrace_exit();}
 #endif
 //#define assert_emit(α, β) try{printf("%s\n%s:%d\n",α,__FILE__,__LINE__);if (!assert_equals_x(emit(α),β)){printf("%s != %s",#α,#β);backtrace_exit();}}catch(chars x){printf("%s\nIN %s",x,α);backtrace_exit();}
