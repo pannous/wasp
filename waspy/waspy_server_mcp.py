3eeb31aa2bec8a7a6f4568781d071856f224c48a
pannous <info@pannous.com>
2025-07-06 16:11:32 +0200

    hallo?

--- waspy/waspy_server_mcp.py
+++ waspy/waspy_server_mcp.py
@@ -1,18 +1,18 @@
 #!gunicorn waspy_server:application
 from flask import Flask, request, redirect, url_for
 from flask_cors import CORS  # pip install flask-cors
-from mcp.server.fastmcp import FastMCP # pip install "mcp[cli]"
+from mcp.server.fastmcp import FastMCP  # pip install "mcp[cli]"
 import threading, queue
 import waspy
 
 register_queue = queue.Queue()
-mcp = FastMCP("waspy", host="0.0.0.0", port=3001, debug=True )  # FastMCP instance for managing tools
+mcp = FastMCP("waspy", host="0.0.0.0", port=3001, debug=True)  # FastMCP instance for managing tools
 
 app = Flask(__name__)
 CORS(app)
 
-wasm_files = {} # name -> filename
-binaries = {} # name -> wasm bytes
+wasm_files = {}  # name -> filename
+binaries = {}  # name -> wasm bytes
 
 form_ = '''
 <form method="post" action="/">
@@ -26,6 +26,7 @@ form_ = '''
 </form>
 '''
 
+
 @app.route('/', defaults={'lib': None, 'func': '_start', 'args': ''})
 @app.route('/<lib>/', defaults={'func': '_start', 'args': ''})
 @app.route('/<lib>/<func>/', defaults={'args': ''})
@@ -34,7 +35,7 @@ def index(lib, func, args):
 	path = request.path
 	if not path or not lib:
 		return form_
-	blocked = ["robots.txt", ".ico", ".png", ".jpg", ".jpeg", ".gif", "wp-", ".php", "setup/","sitemap.xml"]
+	blocked = ["robots.txt", ".ico", ".png", ".jpg", ".jpeg", ".gif", "wp-", ".php", "setup/", "sitemap.xml"]
 	for b in blocked:
 		if b in path: return ""
 	params = {}
@@ -57,7 +58,7 @@ def index(lib, func, args):
 	else:
 		file_path = wasm_files[lib]
 		with open(file_path, "rb") as f:
-				wasm_bytes = f.read()
+			wasm_bytes = f.read()
 	try:
 		ok = waspy.run_wasm(wasm_bytes, params, func=func)
 		return str(ok)
@@ -68,36 +69,9 @@ def index(lib, func, args):
 def fix_params(params, binary=None, func=""):
 	if not params: return {}
 	if not isinstance(params, dict):
-		return {"value2":params}
+		return {"value2": params}
 	return params
 
-def register_mcp(lib, func):
-		print("registering mcp", lib, func)
-		@mcp.tool(f"{lib}.{func}")
-		def inner(arguments={}):
-				# argument_dict= fix_params(arguments, binaries[lib], func)
-				return waspy.run_wasm(binaries[lib], arguments, func=func)
-		description = f"Tool {lib}-{func} registered without description, must be inferred from name and function signature."
-		register_queue.put((inner, f"{lib}-{func}", description))
-
-
-def register_test():
-		@mcp.tool("test-hallo",description="send greetings to a user with provided name")
-		def hallo(name="World"):
-				return f"Hallo {name}!"
-		register_queue.put((lambda:"pong", f"ping", "Respond with 'pong' to a ping request."))
-		register_queue.put((lambda echo:echo, f"echo", "Echo the input back to the user."))
-		register_queue.put((hallo, f"hallo", "Send greetings to a user with provided name."))
-
-
-def register_exports_as_mcp(lib, filename):
-		with open(filename, "rb") as fbin:
-			wasm = fbin.read()
-			binaries[lib] = wasm
-			module, store = waspy.instantiate(wasm)
-			for func in module.exports:
-				if "Func" in str(type(func.type)): # skip isinstance(export, wasmtime._types.MemoryType)
-					register_mcp(lib, func.name)
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -110,18 +84,55 @@ def upload():
 	return redirect(url_for('index') + name)
 
 
+def register_exports_as_mcp(lib, filename):
+	with open(filename, "rb") as fbin:
+		wasm = fbin.read()
+		binaries[lib] = wasm
+		module, store = waspy.instantiate(wasm)
+		for func in module.exports:
+			if "Func" in str(type(func.type)):  # skip isinstance(export, wasmtime._types.MemoryType)
+				register_mcp(lib, func.name)
+
+
+known_mcps = []
+def register_mcp(lib, func):
+	print("registering mcp", lib, func)
+
+	@mcp.tool(f"{lib}.{func}")
+	def inner(arguments={}):
+		# argument_dict= fix_params(arguments, binaries[lib], func)
+		return waspy.run_wasm(binaries[lib], arguments, func=func)
+
+	description = f"Tool {lib}-{func} registered without description, must be inferred from name and function signature."
+	register_queue.put((inner, f"{lib}-{func}", description))
+	known_mcps.append(f"{lib}-{func}")
+
+
+def register_test():
+	@mcp.tool("test-hallo", description="send greetings to a user with provided name")
+	def hallo(name="World"):
+		return f"Hallo {name}!"
+
+	register_queue.put((lambda: "pong", f"ping", "Respond with 'pong' to a ping request."))
+	register_queue.put((lambda echo: echo, f"echo", "Echo the input back to the user."))
+	register_queue.put((hallo, f"hallo", "Send greetings to a user with provided name."))
+
 
 def mcp_server():
-		register_test()  # OK
-		def poll_registers(): # needs to be run in a separate thread
-				while True:
-						func, name, description = register_queue.get()
-						print(f"Registering tool: {name} with description: {description}")
-						# mcp.add_tool(mcp.tool(name,description=description)(func), name, description=description)
-						mcp.add_tool(func, name, description=description)
-		threading.Thread(target=poll_registers, daemon=True).start()
-		# mcp.run(transport="sse") can only use one
-		mcp.run(transport="streamable-http")
+	def poll_registers():  # needs to be run in a separate thread
+		while True:
+			func, name, description = register_queue.get()
+			print(f"Registering tool: {name} with description: {description}")
+			# mcp.add_tool(mcp.tool(name,description=description)(func), name, description=description)
+			mcp.add_tool(func, name, description=description)
+			known_mcps.append(name)
+
+	threading.Thread(target=poll_registers, daemon=True).start()
+	register_test()  # OK
+	# mcp.run(transport="sse") can only use one
+	mcp.run(transport="streamable-http")
+
+
 # 		https://mcp.pannous.com/mcp/ or local http://localhost:3001/mcp/
 
 
@@ -129,7 +140,7 @@ def mcp_server():
 thread = threading.Thread(target=mcp_server)
 thread.start()
 
-application = app  #!gunicorn waspy_server:application
+application = app  # !gunicorn waspy_server:application
 if __name__ == '__main__':
 	print("for LIVE environment, use `gunicorn waspy_server:application`")
 	# threading.Thread(target=lambda: mcp_server(), daemon=True).start()
