74910c35cb64787fb4a9c7cc9ab1d83420d86ef2
pannous <info@pannous.com>
2025-07-09 00:22:05 +0200

    CURRENT TESTS PASSED harder!

--- source/tests.cpp
+++ source/tests.cpp
@@ -3934,6 +3934,8 @@ void testCurrent() {
     // print("testCurrent DEACTIVATED");
     // return;
     print("💡 starting current tests 💡");
+    assert_emit("`hello ${1+1}`", "hello 2");
+
     // assert_emit("fib := it < 2 ? it : fib(it - 1) + fib(it - 2)\nfib(10)", 55);
     // assert_emit("k=(1,2,3);i=1;k#i=4;k#1", 4) // fails linking _ZdlPvm operator delete(void*, unsigned long)
     // assert_emit("i=1;k='hi';k#i", 'h'); // BUT IT WORKS BEFORE!?! be careful with i64 smarty return!
