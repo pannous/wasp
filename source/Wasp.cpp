b718608f97e7afa2422fdfeeaa95b8ce23d02627
pannous <info@pannous.com>
2025-12-12 12:05:18 +0100

    refactor: extract test functions to tests/test_functions.h
    Moved test helper functions from source/Wasp.cpp to tests/test_functions.h:
    - test42() - returns 42
    - test42i(int) - returns 42 + i
    - test42f(float) - returns 42 + f
    - test41ff(float) - returns 41.4 + f (tests default args)
    - not_ok() - error test helper
    
    All functions made static to avoid linkage issues when included in multiple files.
    These functions are used for testing wasm->runtime interaction.
    
    All 252 tests passing.

--- source/Wasp.cpp
+++ source/Wasp.cpp
@@ -1310,30 +1310,7 @@ float group_precedence(char group) {
     return 999;
 }
 
-
-// test functions to check wasm->runtime interaction
-int test42() {
-    return 42;
-}
-
-int test42i(int i) {
-    // used in wasm runtime test
-    return 42 + i;
-}
-
-float test42f(float f) {
-    return 42 + f;
-}
-
-// default args don't work in wasm! (how could they?)
-float test41ff(float f = 0) {
-    return 41.4 + f;
-}
-
-
-void not_ok() {
-    error1(""); // assert_throws test
-}
+#include "../tests/test_functions.h"
 
 #if BACKTRACE
 void handler(int sig) {
