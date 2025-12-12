aa4d4067775ce975d4541999c7befd4bd0c2510b
pannous <info@pannous.com>
2025-12-12 13:56:23 +0100

    253 CURRENT TESTS PASSED
    Angle.cpp =>
    Keywords.cpp REFACTOR

--- source/Keywords.cpp
+++ source/Keywords.cpp
@@ -1,4 +1,5 @@
 
+#include "Angle.h"
 #include "List.h"
 #include "Node.h"
 //const Node LongType("LongType", clazz);
@@ -91,3 +92,139 @@ chars control_flows[] = {"if", "while", "for", "unless", "until", "as soon as", 
 List<String> extra_reserved_keywords = {"func"}; // EXTRA!  // use bool isKeyword(String) !!
 
 //List<Kind> class_kinds = {clazz, prototype, interface, structs};// record see wit
+
+
+
+void fixFunctionNames() {
+    for (String name: functions) {
+        functions[name].name = name;
+    }
+}
+
+void preRegisterFunctions() {
+    functions.clear();
+    //  postpone for simple programs!
+    //    Module &runtime = read_wasm("wasp-runtime.wasm"); // ok, cached!
+    //    runtime.code.needs_relocate = false; // may be set to true depending on main code emitted
+    //
+    //    for (int i = 0; i < runtime.functions.size(); ++i)
+    //        runtime.functions.values[i].is_used = false; // reset after last used, as libraries are shared between tests
+
+    functions["proc_exit"].import();
+    functions["proc_exit"].signature.add(int32t, "exit_code"); // file descriptor
+    //	wasp-runtime.wasm needs to be recompiled too!
+#if WASMEDGE
+    functions["proc_exit"].module = new Module{.name = "wasi_snapshot_preview1"};
+#else
+    functions["proc_exit"].module = new Module{.name="wasi_unstable"};
+#endif
+    functions["fd_write"].import();
+    functions["fd_write"].signature.add(int32t, "fd"); // file descriptor
+    functions["fd_write"].signature.add(pointer, "iovs");
+    functions["fd_write"].signature.add(size32, "iovs_len");
+    functions["fd_write"].signature.add(pointer, "nwritten"); // size_t *  out !
+    functions["fd_write"].signature.returns(int32t);
+    //    functions["fd_write"].module=new Module{.name="wasi"};
+#if WASMEDGE or WASMTIME
+    functions["fd_write"].module = new Module{.name = "wasi_snapshot_preview1"};
+#else
+    functions["fd_write"].module = new Module{.name="wasi_unstable"};
+#endif
+
+    functions["printx"].import();
+    functions["printx"].signature.add(externref, "extern_reference");
+
+    functions["download"].import();
+    functions["download"].signature.add(charp, "url").returns(charp);
+    // todo alias ^^
+    functions["fetch"].import();
+    functions["fetch"].signature.add(charp, "url").returns(charp);
+
+    // DOM functions
+    functions["getDocumentBody"].import().signature.returns(externref);
+    functions["createHtml"].import();
+    functions["createHtml"].signature.add(externref, "parent").add(charp, "innerHtml").returns(externref);
+    functions["createHtmlElement"].import();
+    functions["createHtmlElement"].signature.add(externref, "parent").add(charp, "tag").add(charp, "id").returns(
+        externref);
+
+    functions["addScript"].import().signature.add(charp, "js");
+
+    //#if WASM // no funclets in browser (yet?)
+    //    functions["pow"].import(); PREVENTS reading from pow.wasm file!
+    //    functions["pow"].signature.add(float64).add(float64).returns(float64);
+
+    functions["print"].import();
+    //    functions["print"].signature.add(node).returns(voids);
+    functions["print"].signature.add(smarti64).returns(voids);
+
+    functions["getElementById"].import();
+    functions["getElementById"].signature.add(charp, "id").returns(externref /*!!*/);
+
+    functions["toNode"].import();
+    functions["toNode"].signature.add(referencex, "id").returns(node);
+    // todo: do all these via toNode!
+    functions["toString"].import();
+    functions["toString"].signature.add(referencex, "id").returns(charp);
+    functions["toLong"].import();
+    functions["toLong"].signature.add(referencex, "id").returns(longs);
+    functions["toReal"].import();
+    functions["toReal"].signature.add(referencex, "id").returns(reals);
+
+    // functions["formatLong"].import();
+    // functions["formatLong"].signature.add(longs).returns(strings);
+
+    functions["concat"].import(); // todo load from runtime AGAIN 2025-06-16
+    functions["concat"].signature.add(charp).add(charp).returns(charp);
+
+
+    functions["invokeExternRef"].import();
+    // how to distinguish between functions and properties? 1. no params use getExternRefPropertyValue
+    functions["invokeExternRef"].signature.add(externref).add(charp, "method").add(charp, "params").returns(smarti64);
+
+    //    functions["invokeExternRef"].signature.add(externref).add(strings, "method").add(node, "params").returns(smarti64);
+    //    TODO get pointer of node on stack
+
+    functions["getExternRefPropertyValue"].import(); // for consumption
+    functions["getExternRefPropertyValue"].signature.add(externref, "object").add(charp, "field").returns(smarti64);
+    //	functions["getExternRefPropertyValue"].signature.add(externref).add(charp).returns(stringref); // ⚠️ not yet in webview!
+
+    functions["setExternRefPropertyValue"].import();
+    functions["setExternRefPropertyValue"].signature.
+            add(externref, "object").add(charp, "field").add(smarti64, "value").returns(smarti64);
+
+
+    //	functions["$"].import();
+    //	functions["$"].signature.add(strings).returns(referencex);
+
+    functions["puts"].builtin();
+    functions["puts"].signature.add(stringp).returns(int32t); // stdio conform!!
+
+    functions["len"].builtin(); // via wasp abi len(any)=*(&any)[1]
+    functions["len"].signature.add(array).returns(int32t); // todo any wasp type
+
+    functions["quit"].builtin(); // no args, calls proc_exit(0)
+
+    // TESTS! not useful otherwise!
+    //    functions["square"].import().signature.add(int32).returns(int32);// test only!!
+
+#if MULTI_VALUE
+    functions["wasp_main"].signature.returns(i64).returns(i32);// [result, type32] transparently (no flipped stack order)
+#else
+    //    functions["wasp_main"].signature.returns(i64);
+    functions["wasp_main"].signature.returns(smarti64);
+#endif
+    // check(functions["wasp_main"].name == "wasp_main");
+    // check(functions["wasp_main"].signature.return_types.size() >= 1);
+    //    functions["paint"].import().signature.returns(voids);// paint surface
+    //    functions["init_graphics"].import().signature.returns(pointer);// surface
+
+    // BUILTINS
+    //    functions["nop"].builtin(); NOT A FUNCTION! an op
+    functions["id"].builtin().signature.add(i32t).returns(i32t);
+    functions["modulo_float"].builtin().signature.add(float32t).add(float32t).returns(float32t);
+    functions["modulo_double"].builtin().signature.add(float64t).add(float64t).returns(float64t);
+
+    fixFunctionNames();
+}
+
