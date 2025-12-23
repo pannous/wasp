8c6591e0eb062155bcef9295d730644b02270df7
pannous <info@pannous.com>
2025-12-23 17:40:20 +0100

    273 CURRENT TESTS PASSED WIP

--- source/ffi_dynamic_wrapper.h
+++ source/ffi_dynamic_wrapper.h
@@ -19,7 +19,7 @@ inline WasmEdge_Result ffi_dynamic_wrapper(
     WasmEdge_Value *Out
 ) {
     // Context contains FFIContext with function pointer and signature info
-    FFIMarshaller::FFIContext* ctx = (FFIMarshaller::FFIContext*)context_data;
+    FFIMarshaller::FFIContext *ctx = (FFIMarshaller::FFIContext *) context_data;
 
     if (!ctx || !ctx->function_ptr || !ctx->signature) {
         return WasmEdge_Result_Fail;
@@ -124,7 +124,7 @@ inline WasmEdge_Result ffi_dynamic_wrapper(
 #include "wasmtime.h"
 
 // Single generic wrapper for Wasmtime - handles ALL FFI functions dynamically
-static wasm_trap_t* ffi_dynamic_wrapper_wasmtime(
+static wasm_trap_t *ffi_dynamic_wrapper_wasmtime(
     void *env,
     wasmtime_caller_t *caller,
     const wasmtime_val_t *args,
@@ -133,14 +133,13 @@ static wasm_trap_t* ffi_dynamic_wrapper_wasmtime(
     size_t nresults
 ) {
     // Env contains FFIContext with function pointer and signature info
-    FFIMarshaller::FFIContext* ctx = (FFIMarshaller::FFIContext*)env;
+    FFIMarshaller::FFIContext *ctx = (FFIMarshaller::FFIContext *) env;
 
-    if (!ctx || !ctx->function_ptr || !ctx->signature) {
+    if (!ctx || !ctx->function_ptr || !ctx->signature)
         return NULL; // Or create trap
-    }
 
     // Get wasm memory for string conversions
-    extern void *wasm_memory;  // Defined in wasmtime_runner.cpp
+    extern void *wasm_memory; // Defined in wasmtime_runner.cpp
 
     int param_count = ctx->signature->parameters.size();
 
@@ -150,42 +149,35 @@ static wasm_trap_t* ffi_dynamic_wrapper_wasmtime(
     for (int i = 0; i < param_count; i++) {
         FFIMarshaller::FFIValue arg;
         FFIMarshaller::CType param_type = FFIMarshaller::get_param_ctype(*ctx->signature, i);
-
         switch (param_type) {
             case FFIMarshaller::CType::Int32:
                 arg.i32 = args[i].of.i32;
                 arg.i32 = FFIMarshaller::to_c_int32(arg.i32);
                 break;
-
             case FFIMarshaller::CType::Int64:
                 arg.i64 = args[i].of.i64;
                 arg.i64 = FFIMarshaller::to_c_int64(arg.i64);
                 // print("  arg["s + formatLong(i) + "] i64 = " + formatLong(arg.i64));
                 break;
-
             case FFIMarshaller::CType::Float32:
                 arg.f32 = args[i].of.f32;
                 arg.f32 = FFIMarshaller::to_c_float(arg.f32);
                 // printf("  arg[%d] f32 = %f\n", i, arg.f32);
                 break;
-
             case FFIMarshaller::CType::Float64:
                 arg.f64 = args[i].of.f64;
                 arg.f64 = FFIMarshaller::to_c_double(arg.f64);
                 // printf("  arg[%d] f64 = %f\n", i, arg.f64);
                 break;
-
             case FFIMarshaller::CType::String: {
                 int32_t offset = args[i].of.i32;
                 arg.str = FFIMarshaller::offset_to_c_string(wasm_memory, offset);
                 break;
             }
-
             default:
                 arg.i64 = 0;
                 break;
         }
-
         ffi_args.add(arg);
     }
 
@@ -200,29 +192,24 @@ static wasm_trap_t* ffi_dynamic_wrapper_wasmtime(
             results[0].kind = WASMTIME_I32;
             results[0].of.i32 = FFIMarshaller::from_c_int32(result.i32);
             break;
-
         case FFIMarshaller::CType::Int64:
             results[0].kind = WASMTIME_I64;
             results[0].of.i64 = FFIMarshaller::from_c_int64(result.i64);
             break;
-
         case FFIMarshaller::CType::Float32:
             results[0].kind = WASMTIME_F32;
             results[0].of.f32 = FFIMarshaller::from_c_float(result.f32);
             break;
-
         case FFIMarshaller::CType::Float64:
             results[0].kind = WASMTIME_F64;
             results[0].of.f64 = FFIMarshaller::from_c_double(result.f64);
             break;
-
         case FFIMarshaller::CType::String: {
             int32_t offset = FFIMarshaller::c_string_to_offset(wasm_memory, result.str);
             results[0].kind = WASMTIME_I32;
             results[0].of.i32 = offset;
             break;
         }
-
         default:
             results[0].kind = WASMTIME_I32;
             results[0].of.i32 = 0;
@@ -235,12 +222,12 @@ static wasm_trap_t* ffi_dynamic_wrapper_wasmtime(
 #endif // WASMTIME
 
 // Helper to create FFI context from Wasp Signature
-inline FFIMarshaller::FFIContext* create_ffi_context(
-    class Signature* wasp_sig,
-    void* func_ptr,
-    const String& func_name
+inline FFIMarshaller::FFIContext *create_ffi_context(
+    class Signature *wasp_sig,
+    void *func_ptr,
+    const String &func_name
 ) {
-    FFIMarshaller::FFIContext* ctx = new FFIMarshaller::FFIContext();
+    FFIMarshaller::FFIContext *ctx = new FFIMarshaller::FFIContext();
     ctx->signature = wasp_sig;
     ctx->function_ptr = func_ptr;
     ctx->function_name = func_name;
